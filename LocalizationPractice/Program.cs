66008c349ca06d129321c0d6a305ee2b06e53808
Eric Carlier <eric.carlier@gmail.com>
2022-02-04 23:37:34 +0100

    Localization Practise Init

---
+++ LocalizationPractice/Program.cs
@@ -0,0 +1,12 @@
+using LocalizationPractice;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization(opt => opt.ResourcesPath = "Resources");
+
+await builder.Build().RunAsync();
